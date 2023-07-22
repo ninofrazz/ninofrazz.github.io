f3c57609dd52023dce8d0ea0906a35fcf0238e83
ninofrazzitta94 <140224552+ninofrazz@users.noreply.github.com>
2023-07-22 05:36:41 +0200

    adding new files

---
+++ script.js
@@ -0,0 +1,98 @@
+//  OwlCarousel
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    autoplay: true,
+    smartSpeed: 1000,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    items: 6,
+    dotsEach: true,
+
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+      1200: {
+        items: 6,
+      },
+    },
+  });
+});
+
+new WOW().init();
+
+// get the element
+const text = document.querySelector(".typing-text");
+
+// make a words array
+const words = [
+  "software craftsman.",
+  "creative thinker.",
+  "resourceful troubleshooter.",
+];
+
+// start typing effect
+setTyper(text, words);
+
+function setTyper(element, words) {
+  const LETTER_TYPE_DELAY = 95;
+  const WORD_STAY_DELAY = 2000;
+
+  const DIRECTION_FORWARDS = 0;
+  const DIRECTION_BACKWARDS = 1;
+
+  var direction = DIRECTION_FORWARDS;
+  var wordIndex = 0;
+  var letterIndex = 0;
+
+  var wordTypeInterval;
+
+  startTyping();
+
+  function startTyping() {
+    wordTypeInterval = setInterval(typeLetter, LETTER_TYPE_DELAY);
+  }
+
+  function typeLetter() {
+    const word = words[wordIndex];
+
+    if (direction == DIRECTION_FORWARDS) {
+      letterIndex++;
+
+      if (letterIndex == word.length) {
+        direction = DIRECTION_BACKWARDS;
+        clearInterval(wordTypeInterval);
+        setTimeout(startTyping, WORD_STAY_DELAY);
+      }
+    } else if (direction == DIRECTION_BACKWARDS) {
+      letterIndex--;
+
+      if (letterIndex == 0) {
+        nextWord();
+      }
+    }
+
+    const textToType = word.substring(0, letterIndex);
+
+    element.textContent = textToType;
+  }
+
+  function nextWord() {
+    letterIndex = 0;
+    direction = DIRECTION_FORWARDS;
+    wordIndex++;
+
+    if (wordIndex == words.length) {
+      wordIndex = 0;
+    }
+  }
+}
