496e617c62843222934dfe90d792eda465795eb2
ninofrazzitta94 <140224552+ninofrazz@users.noreply.github.com>
2023-10-03 15:13:24 +0200

    better gifs

--- script.js
+++ script.js
@@ -96,12 +96,3 @@ function setTyper(element, words) {
     }
   }
 }
-
-window.onload = function () {
-  var preloader = document.getElementById("preloader");
-  var content = document.getElementById("content");
-
-  // Hide the preloader and show the content
-  preloader.style.display = "none";
-  content.style.display = "block";
-};
